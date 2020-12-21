128d56edcbc9ec7d221eaea3c966c3f7ceee975c
Rohit Chalamala <65873746+RChalamala01@users.noreply.github.com>
2020-12-20 22:44:02 -0600

    init commit

---
+++ code/rubikscubesolver/cmake-build-debug/CMakeFiles/rubikscubesolver.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rubikscubesolver.dir/main.cpp.o"
+  "rubikscubesolver"
+  "rubikscubesolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rubikscubesolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
